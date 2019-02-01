d8eed33883a1500d8d9a47efcbc1ece0f6780311
Robert Tulley <johnathansherbert@gmail.com>
2019-01-31 20:02:17 -0700

    gitignore

--- install.lua
+++
@@ -1,4 +0,0 @@
--- Old installer code has been removed.
--- Use the AeroGameFramework plugin to install and update the framework.
-
-warn("Please use the installer through the AeroGameFramework plugin")
